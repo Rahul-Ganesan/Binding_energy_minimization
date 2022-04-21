4ece8689c0468981f03214640cdfe5ede195154b
Akshaya Jeyapraksh <86668046+AkshayaJeyaprakash@users.noreply.github.com>
2022-04-21 14:29:06 +0530

    Add files via upload

---
+++ StD.m
@@ -0,0 +1,47 @@
+function min = StD(f)
+syms b phi theta
+% initial guess vaules
+x(1) = 1000;
+y(1) = -200;
+z(1) = 50;
+
+% gradient computation
+df_dx = diff(f,b);
+df_dy = diff(f,phi);
+df_dz = diff(f,theta);
+g0 = [subs(df_dx, [b,phi,theta], [x(1),y(1),z(1)]) 
+      subs(df_dy, [b,phi,theta], [x(1),y(1),z(1)]) 
+      subs(df_dz, [b,phi,theta], [x(1),y(1),z(1)]) ];
+
+% search direction 
+d = -g0;
+
+% iterations
+for i = 1:4
+    I = [x(i),y(i),z(i)];
+    syms t 
+    g = subs(f, [b,phi,theta], [x(i)+d(1)*t,y(i)+t*d(2),z(i)+t*d(3)]);
+    dg_dt = diff(g,t);
+    t = solve(dg_dt,t);
+    x(i+1) = I(1)+t*d(1); % New x value
+    y(i+1) = I(2)+t*d(2); % New y value
+    z(i+1) = I(3)+t*d(3); % New z value
+    g_o = [subs(df_dx, [b,phi,theta], [x(i),y(i),z(i)]) 
+           subs(df_dy, [b,phi,theta], [x(i),y(i),z(i)])
+           subs(df_dz, [b,phi,theta], [x(i),y(i),z(i)])];
+    i = i+1;
+    g_1 = [subs(df_dx, [b,phi,theta], [x(i),y(i),z(i)]) 
+           subs(df_dy, [b,phi,theta], [x(i),y(i),z(i)])
+           subs(df_dz, [b,phi,theta], [x(i),y(i),z(i)])]; % Updated Gradient
+    d=-(g_1);
+end
+Iter = 1:i;
+X_coordinate = x';
+Y_coordinate = y';
+Z_coordinate = z';
+Iterations = Iter';
+T = table(Iterations,X_coordinate,Y_coordinate,Z_coordinate);
+
+min= [ x(i), y(i), z(i), subs(f,[b,phi,theta], [x(i),y(i),z(i)])];
+end
+
